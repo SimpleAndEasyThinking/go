a8a004bfa4aacc0edcbe7f24276d30834bcb61a4
任韩 <q1429700631@outlook.com>
2024-01-19 22:02:03 +0800

    测试Hello

--- go/hello/hello.go
+++ go/hello/hello.go
@@ -2,10 +2,28 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name
+const frenchHelloPrefix = "Spanish"
+const helloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	return greetingPrefix(language) + name
+}
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case "french":
+		prefix = frenchHelloPrefix
+	case "spanish":
+		prefix = spanishHelloPrefix
+	default:
+		prefix = helloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
